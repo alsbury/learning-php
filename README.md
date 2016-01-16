d12e5cc803f987a81ffe9fc52d215a4532bd4eb3
David Alsbury <david@alsbury.com>
2016-01-16 11:09:51 -0800

    Updates to readme

--- README.md
+++ README.md
@@ -2,15 +2,61 @@
 
 This is intended to be an turn-key environment for learning PHP on a Mac. 
 
-## Install Vagrant
+# Getting Started
+
+### Install Vagrant
 
 [Getting Started](https://docs.vagrantup.com/v2/getting-started/index.html)
 
-## Install VirtualBox
+### Install VirtualBox
 
 [VirtualBox](https://www.virtualbox.org/)
 
-## Install an IDE
+### Create Your Environment
 
-- PhpStorm - Best PHP IDE, but also costs $$. If you are serious, it's well worth the investment. https://www.jetbrains.com/phpstorm/
-- Netbeans - Another great PHP IDE
+First you will need to clone this project:
+
+```
+git clone git@github.com:alsbury/learning-php.git
+```
+
+Then change directory to the repo you just checkout out:
+
+```
+cd learning-php
+```
+
+Then build your VM:
+
+```
+vagrant up
+```
+
+Then SSH into your new VM:
+```
+vagrant ssh
+```
+
+To exit the VM:
+
+```
+exit
+```
+
+To halt the VM:
+
+```
+vagrant halt
+```
+
+To Destroy your VM:
+```
+vagrant destroy
+```
+
+### Install an IDE
+
+- [PhpStorm](https://www.jetbrains.com/phpstorm/) - Best PHP IDE, but also costs $$. If you are serious, it's well worth the investment. 
+- [Netbeans](https://netbeans.org/) - Another great PHP IDE. Not quite as nice as PhpStorm, but it's free.
+
+# Start Learning
--- web/index.php
+++ web/index.php
@@ -1,2 +1,36 @@
-<?php
-echo 'Woot';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Learning PHP</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <h1>Learning PHP</h1>
+    <p>
+        Create a new project by adding it to the web/projects folder. It's probably best to create a new folder for each thing you are learning.
+    </p>
+    <a href="/info.php" class="btn btn-default" role="button">PHP Info Page</a>
+
+</div>
+
+<!--    <a href="https://github.com/alsbury/learning-php">Learning PHP on Github</a>-->
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+</html>
